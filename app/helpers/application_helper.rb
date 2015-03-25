747486af874475614bf797eb660bf65516777999
Chris Carter <chris.carter@unboxedconsulting.com>
2015-03-25 17:23:34 +0000

    Fix flash messages to be consistent with DSDS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,29 @@
 module ApplicationHelper
-  def flash_messages(opts = {})
-    flash.to_h.slice('notice', 'alert').each do |msg_type, message|
-      concat(content_tag(:div, message, class: "#{msg_type}-summary") do
-        content_tag(:p, message) do
-          concat safe_join(Array(message), tag(:br))
-        end
-      end)
+  def flash_messages
+    capture do
+      flash.each do |key, msg|
+        concat flash_message(key, msg)
+      end
     end
-    nil
   end
 
-  def list_errors(object, field_name)
-    if object.errors.any?
-      unless object.errors.messages[field_name].blank?
-        content_tag :ul do
-          object.errors.messages[field_name].collect do |field|
-            concat(content_tag(:li, field))
-          end
-        end
+  def flash_message(key, msg)
+    content_tag(:div, class: "#{key}-summary") do
+      content_tag :p, msg
+    end
+  end
+
+  def object_error_messages(active_model_messages)
+    content_tag(:ul) do
+      active_model_messages.each do |field_name, field_messages|
+        concat errors_for_field(field_name, field_messages)
       end
+    end
+  end
+
+  def errors_for_field field_name, field_messages
+    content_tag :li do
+      "#{t(field_name)}: #{field_messages.join(', ')}".html_safe
     end
   end
 end
