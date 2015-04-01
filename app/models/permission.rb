dc99fb65c5daab3d72dac0e7850e4e0ba15e3a0e
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-04-01 10:37:58 +0100

    Validate uniqueness of permissions
    We just want to stop there being duplicate
    permission instances. We might need to write
    a custom validator for this later because the
    error message for this validation will be attached
    to the `user_id` field, where it probably makes
    more sense to be on base.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -4,5 +4,6 @@ class Permission < ActiveRecord::Base
   belongs_to :government_application
   belongs_to :organisation
 
-  validates :government_application, :role, presence: true
+  validates :user, :role, :government_application, presence: true
+  validates_uniqueness_of :user_id, scope: [:role_id, :government_application_id, :organisation_id]
 end
