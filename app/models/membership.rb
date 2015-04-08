d07cfb0d9e9a509767894953427c686bc7635930
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-04-08 11:00:02 +0100

    Validate against duplicate memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,6 @@ class Membership < ActiveRecord::Base
   belongs_to :profile
   belongs_to :organisation
 
+  validates_uniqueness_of :profile_id, scope: :organisation_id
   validates_presence_of :profile, :organisation
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe Membership do
   describe 'validations' do
     specify { expect(subject).to validate_presence_of(:profile) }
     specify { expect(subject).to validate_presence_of(:organisation) }
+
+    specify do
+      expect(subject).to validate_uniqueness_of(:profile_id).
+        scoped_to(:organisation_id)
+    end
   end
 end
