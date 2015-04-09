6b83ecfe34733b465657f53f2b6aa422a84ca8df
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-04-09 14:28:36 +0100

    Ran last migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330094216) do
+ActiveRecord::Schema.define(version: 20150409131031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20150330094216) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "mobile"
   end
 
   create_table "permissions", force: :cascade do |t|
@@ -101,6 +102,7 @@ ActiveRecord::Schema.define(version: 20150330094216) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "mobile"
   end
 
   create_table "roles", force: :cascade do |t|
