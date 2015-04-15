0f4984dad5363e1efbe00dddd69b18c08e63850c
Kips Davenport <patrick.davenport@unboxedconsulting.com>
2015-04-15 11:05:31 +0100

    remove government_application from seeds

--- db/seeds/roles-permissions.rb
+++ db/seeds/roles-permissions.rb
@@ -8,29 +8,29 @@ service_app = GovernmentApplication.all.select { |ga| ga.oauth_application.name 
 
 Permission.where(role: role,
                  user: User.where(email: "user2@example.com").first,
-                 government_application: service_app,
+                 application: service_app,
                  organisation: Organisation.where(slug: "example-org".first)).first_or_create
 Permission.where(role: role,
                  user: User.where(email: "user2@example.com").first,
-                 government_application: rota_app,
+                 application: rota_app,
                  organisation: Organisation.where(slug: "example-org".first)).first_or_create
 
 (1..5).each do |i|
   Permission.where(role: cso_role,
                    user: User.where(email: "cso#{i}@example.com").first,
-                   government_application: service_app,
+                   application: service_app,
                    organisation: Organisation.where(slug: "custody-suite").first
                   ).first_or_create
 
   Permission.where(role: cco_role,
                    user: User.where(email: "cco#{i}@example.com").first,
-                   government_application: service_app,
+                   application: service_app,
                    organisation: Organisation.where(slug: "capita").first,
                   ).first_or_create
 
   Permission.where(role: solicitor_role,
                    user: User.where(email: "solicitor#{i}@example.com").first,
-                   government_application: service_app,
+                   application: service_app,
                    organisation: Organisation.where(slug: "law-firm").first
                   ).first_or_create
 end
