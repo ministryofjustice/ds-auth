1d4deaa3e61c7fbd6fe320a56628b71e8c3c7d40
Jakub Novotny <novotny.jakub@gmail.com>
2015-04-21 16:46:32 +0100

    Add .collection_from_hash method to Organisation model

--- drs-auth_client/lib/drs/auth_client/models/organisation.rb
+++ drs-auth_client/lib/drs/auth_client/models/organisation.rb
@@ -16,6 +16,16 @@ module Drs
             new(hash[:organisation])
           end
         end
+
+        def self.collection_from_hash(hash)
+          organisations = []
+
+          if hash.has_key?(:organisations)
+            organisations = hash[:organisations].map { |attributes| new(attributes) }
+          end
+
+          organisations
+        end
       end
     end
   end
--- drs-auth_client/spec/lib/drs/auth_client/models/organisation_spec.rb
+++ drs-auth_client/spec/lib/drs/auth_client/models/organisation_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Drs::AuthClient::Models::Organisation do
 
-  let(:attributes) { { uid: 'UID', non_existent: 'NON EXISTENT'} }
-  subject(:organisation) { described_class.new(attributes)}
+  let(:attributes) { {uid: 'UID', non_existent: 'NON EXISTENT'} }
+  subject(:organisation) { described_class.new(attributes) }
 
   describe '#initialize' do
 
@@ -32,6 +32,34 @@ RSpec.describe Drs::AuthClient::Models::Organisation do
       let(:hash) { {wrong_prefix: attributes} }
 
       it { is_expected.to be nil }
+    end
+  end
+
+  describe '.collection_from_hash' do
+    subject { described_class.collection_from_hash(hash) }
+
+    context 'when the data is correctly prefixed based on the model name' do
+      let(:other_attributes) { {uid: 'OTHER UID'}}
+      let(:hash) { {organisations: [attributes, other_attributes]} }
+
+      it { is_expected.to be_a(Array) }
+
+      it 'has all the organisations' do
+        expect(subject.size).to be 2
+      end
+
+      it 'all organisations have their attributes assigned' do
+        expect(subject[0].uid).to eql(attributes[:uid])
+        expect(subject[1].uid).to eql(other_attributes[:uid])
+      end
+    end
+
+    context 'for an incorrect data prefix' do
+      let(:hash) { { wrong_prefix: [attributes]}}
+
+      it { is_expected.to be_a(Array)}
+
+      it { is_expected.to be_empty }
     end
   end
 end
