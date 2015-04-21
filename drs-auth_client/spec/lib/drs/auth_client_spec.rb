1d83b154a88ec44678f4e384f3d64547aa481e91
Jakub Novotny <novotny.jakub@gmail.com>
2015-04-21 14:15:20 +0100

    Add rspec setup for the gem

---
+++ drs-auth_client/spec/lib/drs/auth_client_spec.rb
@@ -0,0 +1,7 @@
+module Drs
+  describe AuthClient do
+    it 'true is true' do
+      expect(true).to be true
+    end
+  end
+end
---
+++ drs-auth_client/spec/spec_helper.rb
@@ -0,0 +1,14 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+
+  config.disable_monkey_patching!
+  config.order = :random
+end
