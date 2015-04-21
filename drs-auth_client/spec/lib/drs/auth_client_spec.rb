ca3f583f31cb2a36ceea24cf560093757528417a
Jakub Novotny <novotny.jakub@gmail.com>
2015-04-21 14:40:52 +0100

    Remove dummy tests

--- drs-auth_client/spec/lib/drs/auth_client_spec.rb
+++ drs-auth_client/spec/lib/drs/auth_client_spec.rb
@@ -1,11 +1,4 @@
 module Drs
   describe AuthClient do
-    it 'true is true' do
-      expect(true).to be true
-    end
-
-    it 'should fail the ci build' do
-      expect(false).to be true
-    end
   end
 end
