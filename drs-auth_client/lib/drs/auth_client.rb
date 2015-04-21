02f199fd0755c4185c8fb9bed6e268ebdf2cdc4e
Jakub Novotny <novotny.jakub@gmail.com>
2015-04-21 14:53:30 +0100

    Allow api host and version to be configured on the AuthClient module

--- drs-auth_client/lib/drs/auth_client.rb
+++ drs-auth_client/lib/drs/auth_client.rb
@@ -2,5 +2,12 @@ require 'drs/auth_client/version'
 
 module Drs
   module AuthClient
+    class << self
+      attr_accessor :host, :version
+
+      def configure
+        yield self
+      end
+    end
   end
 end
--- drs-auth_client/spec/lib/drs/auth_client_spec.rb
+++ drs-auth_client/spec/lib/drs/auth_client_spec.rb
@@ -1,4 +1,21 @@
+require_relative '../../../lib/drs/auth_client'
+
 module Drs
   describe AuthClient do
+    [:host, :version].each do |attr|
+      it "allows #{attr} to be set" do
+        described_class.respond_to?("#{attr}=")
+      end
+
+      it "allows #{attr} to be read" do
+        described_class.respond_to?(attr)
+      end
+    end
+
+    describe '.configure' do
+      it 'yields with passing itself as an argument' do
+        expect { |block| described_class.configure(&block) }.to yield_with_args(described_class)
+      end
+    end
   end
 end
