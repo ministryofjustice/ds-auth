0ac288ecc3303a1147628e39ff93bc11f1ab4e95
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-21 15:53:12 +0100

    Add parent and child associations on Organisation and ensure integrity

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,7 +3,21 @@ class Organisation < ActiveRecord::Base
   has_many :memberships
   has_many :profiles, through: :memberships
 
+  has_many :sub_organisations, class_name: "Organisation", foreign_key: "parent_organisation_id"
+  belongs_to :parent_organisation, class_name: "Organisation"
+
   validates :slug, :name, :organisation_type, presence: true
+  validate :no_circular_references
 
   scope :by_name, -> { order(name: :asc) }
+
+  private
+
+  def no_circular_references(organisation=self)
+    if organisation.parent_organisation == self
+      errors.add(:parent_organisation, 'cannot cause circular references')
+    elsif organisation.parent_organisation.present?
+      no_circular_references(organisation.parent_organisation)
+    end
+  end
 end
