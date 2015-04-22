dc63f55b0acaec94d7b61ba416396f68729b16a5
Jakub Novotny <novotny.jakub@gmail.com>
2015-04-22 15:55:10 +0100

    Introduce Base model and pull all generic methods to it

--- drs-auth_client/lib/drs/auth_client/models/organisation.rb
+++ drs-auth_client/lib/drs/auth_client/models/organisation.rb
@@ -1,31 +1,10 @@
+require_relative 'base'
+
 module Drs
   module AuthClient
     module Models
-      class Organisation
+      class Organisation < Base
         attr_accessor :uid
-
-        def initialize(attributes)
-          attributes.each do |key, value|
-            writter = "#{key}="
-            send(writter, value) if respond_to?(writter)
-          end
-        end
-
-        def self.from_hash(hash)
-          if hash.has_key?(:organisation)
-            new(hash[:organisation])
-          end
-        end
-
-        def self.collection_from_hash(hash)
-          organisations = []
-
-          if hash.has_key?(:organisations)
-            organisations = hash[:organisations].map { |attributes| new(attributes) }
-          end
-
-          organisations
-        end
       end
     end
   end
