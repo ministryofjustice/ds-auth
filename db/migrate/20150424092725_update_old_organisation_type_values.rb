d764e9ad87fec8c8364cf399f399b3ac9627104d
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-24 13:40:14 +0100

    Update migration to work with newer constant value

--- db/migrate/20150424092725_update_old_organisation_type_values.rb
+++ db/migrate/20150424092725_update_old_organisation_type_values.rb
@@ -1,6 +1,6 @@
 class UpdateOldOrganisationTypeValues < ActiveRecord::Migration
   def up
-    organisations_to_update = Organisation.where.not(organisation_type: Organisation::ORGANISATION_TYPES.values)
+    organisations_to_update = Organisation.where.not(organisation_type: Organisation::ORGANISATION_TYPES)
     organisations_to_update.each do |o|
       o.update_attribute(:organisation_type, "custody_suite")
     end
