855ccda9e804dfb04d6daf32f0d6ea62f5fed3c4
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-24 10:31:13 +0100

    Add migration to ensure that all organisation types have valid values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421140404) do
+ActiveRecord::Schema.define(version: 20150424092725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
