c3a1381cb3fbf06bc10bcf3b7fcce0851a36f87b
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-24 10:47:54 +0100

    Move organisation_type translation into a helper

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -6,4 +6,8 @@ module OrganisationsHelper
   def options_for_parent_organisation
     Organisation.all.collect { |o| [ o.name, o.id ] }
   end
+
+  def organisation_type(organisation)
+    t("organisation_types.#{@organisation.organisation_type}")
+  end
 end
