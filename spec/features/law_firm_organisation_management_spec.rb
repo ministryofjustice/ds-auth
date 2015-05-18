4c4f27cd006f909f652f8de6691e9511aa98c798
ansonK <ansonkelly@gmail.com>
2015-05-18 16:44:17 +0100

    Add supplier_number field to law_firm type Organisations

---
+++ spec/features/law_firm_organisation_management_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.feature "Users managing law firm type organisations" do
+  let(:user) { create(:user) }
+  let!(:organisation) { create(:organisation, :with_profiles_and_users, profile_count: 3) }
+
+  before do
+    login_as_user user.email
+  end
+
+  specify "can create a new law firm with a supplier number", js: true do
+    visit organisations_path
+
+    click_link "New Organisation"
+
+    fill_in "Name", with: "my law firm"
+    fill_in "Slug", with: "my-law-firm"
+
+    select "Law firm", from: "Organisation type"
+
+    fill_in "Supplier number", with: "AABBCC12345678"
+
+    click_button "Create Organisation"
+
+    expect(current_path).to eq(organisations_path)
+    expect(page).to have_content "Organisation successfully created"
+    expect(page).to have_content "my law firm"
+
+    within "#my-law-firm-row" do
+      click_link "Show"
+    end
+
+    expect(page).to have_content "Supplier Number: AABBCC12345678"
+  end
+end
