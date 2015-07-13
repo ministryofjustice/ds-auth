64d7f1c58ba20241cef77059b47b14cad9c4066a
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-13 15:31:36 +0100

    Remove Application seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-%w[users organisations applications memberships-permissions].each do |seed|
+%w[users organisations memberships-permissions].each do |seed|
   puts "Seeding #{seed.titleize.humanize}..."
   load "#{Rails.root}/db/seeds/#{seed}.rb"
 end
