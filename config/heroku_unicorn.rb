2d36cf0e9c45ef4a8faa19d6392d61047e5f0e6e
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-13 13:59:22 +0100

    Appease Rubocop

--- config/heroku_unicorn.rb
+++ config/heroku_unicorn.rb
@@ -3,9 +3,9 @@ timeout 30
 preload_app true
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  Signal.trap "TERM" do
+    puts "Unicorn master intercepting TERM and sending myself QUIT instead"
+    Process.kill "QUIT", Process.pid
   end
 
   defined?(ActiveRecord::Base) and
@@ -13,8 +13,8 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  Signal.trap "TERM" do
+    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
   end
 
   defined?(ActiveRecord::Base) and
