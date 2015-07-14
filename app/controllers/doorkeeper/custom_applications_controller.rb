963f6f8685569846b5c81b953f3e8ffbe9fa661d
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-14 15:06:54 +0100

    Remove extra rescue from nil (urgh)

--- app/controllers/doorkeeper/custom_applications_controller.rb
+++ app/controllers/doorkeeper/custom_applications_controller.rb
@@ -6,7 +6,7 @@ module Doorkeeper
       if params.respond_to?(:permit)
         params.require(:doorkeeper_application).permit(:name, :redirect_uri, :handles_own_authorization, :scopes)
       else
-        params[:doorkeeper_application].slice(:name, :redirect_uri, :handles_own_authorization, :scopes) rescue nil
+        (params[:doorkeeper_application] || {}).slice(:name, :redirect_uri, :handles_own_authorization, :scopes)
       end
     end
   end
