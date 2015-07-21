0f3b616bad7d431b9076491d97c22eb72f19ffd4
Chris Carter <chris.carter@unboxedconsulting.com>
2015-07-21 16:11:41 +0100

    Remove seeds files which are no longer required

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,1 @@
-User.where(email: "admin@example.com").first_or_create!(
-  password: "password",
-  name: "Default Webops user",
-  address: "Fake Street",
-  postcode: "POSTCODE",
-  email: "webops@example.com",
-  telephone: "01234567890",
-  mobile: "07123456789",
-  is_webops: true
-)
+# Add seeds below here.
--- db/seeds/users.rb
+++
@@ -1,9 +0,0 @@
-User.where(email: "admin@example.com").first_or_create!(
-  password: "password",
-  name: "Default Webops user",
-  address: "Fake Street",
-  postcode: "POSTCODE",
-  email: "webops@example.com",
-  telephone: "01234567890",
-  mobile: "07123456789"
-)
